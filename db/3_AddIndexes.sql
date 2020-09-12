d1980f246a8dda1cfe3b3973c0854d04f941f720
Tatsuro Furusawa <fur.sax.smallworld@gmail.com>
2020-09-12 19:50:25 +0900

    Revert "fix"
    This reverts commit f1a75d7900fc3287b18d1ead999ecc69c8bbea7d.

--- db/3_AddIndexes.sql
+++ db/3_AddIndexes.sql
@@ -1,6 +1,8 @@
 use `isuumo`;
 
 ALTER TABLE `isuumo`.`chair` ADD INDEX idx_chair_price_id (`price`, `id`);
+ALTER TABLE `isuumo`.`chair` ADD INDEX idx_chair_price_id (`price`, `id`);
+ALTER TABLE `isuumo`.`chair` ADD INDEX idx_chair_price (`price`);
 ALTER TABLE `isuumo`.`chair` ADD INDEX idx_chair_height (`height`);
 ALTER TABLE `isuumo`.`chair` ADD INDEX idx_chair_width (`width`);
 ALTER TABLE `isuumo`.`chair` ADD INDEX idx_chair_depth (`depth`);
@@ -11,3 +13,4 @@ ALTER TABLE `isuumo`.`estate` ADD INDEX idx_estate_rent_id (`rent`, `id`);
 ALTER TABLE `isuumo`.`estate` ADD INDEX idx_estate_latitude_longitude_popularity (`latitude`, `longitude`, `popularity`);
 ALTER TABLE `isuumo`.`estate` ADD INDEX idx_estate_door_height (`door_height`);
 ALTER TABLE `isuumo`.`estate` ADD INDEX idx_estate_door_width (`door_width`);
+ALTER TABLE `isuumo`.`estate` ADD INDEX idx_estate_rent (`rent`);

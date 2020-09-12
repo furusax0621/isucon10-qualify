9a7f206c8f31094bdfb07564e91a391ace513ae3
Tatsuro Furusawa <fur.sax.smallworld@gmail.com>
2020-09-12 19:49:59 +0900

    Revert "chan size"
    This reverts commit 2784f1536ff966ec8011e32c4512d5249ab760f6.

--- webapp/main.go
+++ webapp/main.go
@@ -266,7 +266,7 @@ func main() {
 		fmt.Println(http.ListenAndServe("0.0.0.0:6060", nil))
 	}()
 
-	recordsChan := make(chan [][]string, 5)
+	recordsChan := make(chan [][]string)
 	go func() {
 		for {
 			records := <-recordsChan

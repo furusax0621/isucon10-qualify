c4e6933752c740393a2df074a6366e1bd10492fa
Hiroki Ito <hiroki.ito@fuller.co.jp>
2020-09-12 15:22:19 +0900

    No index chair.

--- db/3_AddIndexes.sql
+++ db/3_AddIndexes.sql
@@ -1,4 +1,3 @@
 use `isuumo`;
 
-ALTER TABLE `isuumo`.`chair` ADD INDEX idx_chair_stock_price_id (`stock`, `price`, `id`);
 ALTER TABLE `isuumo`.`estate` ADD INDEX idx_estate_rent_id (`rent`, `id`);

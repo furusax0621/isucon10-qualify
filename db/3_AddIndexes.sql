823509318ebe2bb5bb8281d632f8cb66dbc48aef
Tatsuro Furusawa <fur.sax.smallworld@gmail.com>
2020-09-12 19:50:31 +0900

    Revert "Fix"
    This reverts commit 18614e358ec9077175023d805a5eaa57f04b37d0.

--- db/3_AddIndexes.sql
+++ db/3_AddIndexes.sql
@@ -2,15 +2,15 @@ use `isuumo`;
 
 ALTER TABLE `isuumo`.`chair` ADD INDEX idx_chair_price_id (`price`, `id`);
 ALTER TABLE `isuumo`.`chair` ADD INDEX idx_chair_price_id (`price`, `id`);
-ALTER TABLE `isuumo`.`chair` ADD INDEX idx_chair_price (`price`);
-ALTER TABLE `isuumo`.`chair` ADD INDEX idx_chair_height (`height`);
-ALTER TABLE `isuumo`.`chair` ADD INDEX idx_chair_width (`width`);
-ALTER TABLE `isuumo`.`chair` ADD INDEX idx_chair_depth (`depth`);
-ALTER TABLE `isuumo`.`chair` ADD INDEX idx_chair_kind (`kind`);
-ALTER TABLE `isuumo`.`chair` ADD INDEX idx_chair_color (`color`);
+ALTER TABLE `isuumo`.`chair` ADD INDEX idx_chair_price (`price`)
+ALTER TABLE `isuumo`.`chair` ADD INDEX idx_chair_height (`height`)
+ALTER TABLE `isuumo`.`chair` ADD INDEX idx_chair_width (`width`)
+ALTER TABLE `isuumo`.`chair` ADD INDEX idx_chair_depth (`depth`)
+ALTER TABLE `isuumo`.`chair` ADD INDEX idx_chair_kind (`kind`)
+ALTER TABLE `isuumo`.`chair` ADD INDEX idx_chair_color (`color`)
 
 ALTER TABLE `isuumo`.`estate` ADD INDEX idx_estate_rent_id (`rent`, `id`);
 ALTER TABLE `isuumo`.`estate` ADD INDEX idx_estate_latitude_longitude_popularity (`latitude`, `longitude`, `popularity`);
-ALTER TABLE `isuumo`.`estate` ADD INDEX idx_estate_door_height (`door_height`);
-ALTER TABLE `isuumo`.`estate` ADD INDEX idx_estate_door_width (`door_width`);
-ALTER TABLE `isuumo`.`estate` ADD INDEX idx_estate_rent (`rent`);
+ALTER TABLE `isuumo`.`estate` ADD INDEX idx_estate_door_height (`door_height`)
+ALTER TABLE `isuumo`.`estate` ADD INDEX idx_estate_door_width (`door_width`)
+ALTER TABLE `isuumo`.`estate` ADD INDEX idx_estate_rent (`rent`)

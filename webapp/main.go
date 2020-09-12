fad543b693f1e0fd9c14d7ad1206b991cd765c4d
Hiroki Ito <hiroki.ito@fuller.co.jp>
2020-09-12 19:43:17 +0900

    Add records to cache in handler.

--- webapp/main.go
+++ webapp/main.go
@@ -291,10 +291,6 @@ func main() {
 					log.Errorf("failed to commit tx: %v", err)
 					return
 				}
-
-				for _, r := range records {
-					estateCashe.Store(r.ID, r)
-				}
 			}()
 		}
 	}()
@@ -786,6 +782,11 @@ func postEstate(c echo.Context) error {
 		}
 		estates = append(estates, estate)
 	}
+
+	for _, e := range estates {
+		estateCashe.Store(e.ID, e)
+	}
+
 	recordsChan <- estates
 
 	return c.NoContent(http.StatusCreated)
